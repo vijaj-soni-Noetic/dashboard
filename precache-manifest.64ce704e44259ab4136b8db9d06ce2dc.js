32b2a2994b4191f973dc61e6cd6ffa38c165dae4
vijay-soni-Noetic <vkvijaygupta3@gmail.com>
2020-07-28 09:16:22 +0530

    Updates

---
+++ precache-manifest.64ce704e44259ab4136b8db9d06ce2dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "135fa3aaeeb69f053860bf28615e0c4d",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "dba3bb26150a450a43d0",
+    "url": "/admin/static/css/main.ece41771.chunk.css"
+  },
+  {
+    "revision": "66b2785992df91fbc275",
+    "url": "/admin/static/js/2.b83f3ce4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/admin/static/js/2.b83f3ce4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dba3bb26150a450a43d0",
+    "url": "/admin/static/js/main.df835e30.chunk.js"
+  },
+  {
+    "revision": "c9dc533f55d56231b6a4",
+    "url": "/admin/static/js/runtime-main.f9c1702f.js"
+  },
+  {
+    "revision": "eab8d88266f5d9969c5d7560abae8d3c",
+    "url": "/admin/static/media/profilelg.eab8d882.png"
+  },
+  {
+    "revision": "03f51271cafe1bc0efa6c97b2ec2e0a6",
+    "url": "/admin/static/media/profilesm.03f51271.png"
+  }
+]);
